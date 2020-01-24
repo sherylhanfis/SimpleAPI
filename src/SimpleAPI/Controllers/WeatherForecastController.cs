bad389af9799b2747c3f6e672abedcc2eadfe6c8
Han <Zhinan.Han@FISGLOBAL.COM>
2020-01-24 16:24:19 -0500

    1st commit

---
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Http;
+
+
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/student/5
+        public ActionResult<string> Get(int id)
+        {
+            return "Sheryl Han";
+        }
+
+        // POST: api/student
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/student/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/student/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
